32a3c91a00c9b18819db25e9c11ddfb66cae2d27
EkaterinaBakulina <115363479+EkaterinaBakulina@users.noreply.github.com>
2023-01-03 18:25:22 +0300

    Лабораторная работа 2
    Здравствуйте! Я хотела бы поделиться трудностью, с которой столкнулась при выполнении лабораторной работы. При решении задания main (Library) у меня почему-то подчёркивает красным цветом "from pydantic import BaseModel", хотя я устанавливала пакет pydantic в терминале (pip install pydantic). Из-за этого код не работает должным образом, и я не могу понять, правильно я его написала или нет. Была бы Вам крайне благодарна, если бы Вы смогли мне подсказать, в чём моя ошибка...

---
+++ Лабораторная работа 2/main (Library).py
@@ -0,0 +1,58 @@
+from typing import Optional
+from pydantic import BaseModel
+
+BOOKS_DATABASE = [
+    {
+        "id": 1,
+        "name": "test_name_1",
+        "pages": 200,
+    },
+    {
+        "id": 2,
+        "name": "test_name_2",
+        "pages": 400,
+    }
+]
+
+
+class Book:
+    def __init__(self, id_: int, name: str, pages: int):
+        self.id_ = id_
+        self.name = name
+        self.pages = pages
+
+    def __str__(self):
+        return f'Книга "{self.name}"'
+
+    def __repr__(self):
+        return f'Book(id_={self.id_!r}, name={self.name!r}, pages={self.pages!r})'
+
+
+class Library(BaseModel):
+    books: Optional[list]
+    id_number = 0
+
+    @classmethod
+    def get_next_book_id(cls):
+        cls.id_number += 1
+
+    @classmethod
+    def get_index_by_book_id(cls, id_):
+        for i, book in enumerate(cls.books):
+            if book["id"] != id_:
+                raise ValueError("Книги с запрашиваемым id не существует")
+            else:
+                return i
+
+
+if __name__ == '__main__':
+    empty_library = Library()  # инициализируем пустую библиотеку
+    print(empty_library.get_next_book_id())  # проверяем следующий id для пустой библиотеки
+
+    list_books = [
+        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+    ]
+    library_with_books = Library(books=list_books)  # инициализируем библиотеку с книгами
+    print(library_with_books.get_next_book_id())  # проверяем следующий id для непустой библиотеки
+
+    print(library_with_books.get_index_by_book_id(1))  # проверяем индекс книги с id = 1
